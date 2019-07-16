4c14643909e5bffdfbb337795746f78c5dce08ca
Eric Tobin <eatobin@gmail.com>
2019-07-16 08:32:57 -0500

    cider format

--- src/total/borrower.clj
+++ src/total/borrower.clj
@@ -1,7 +1,7 @@
 (ns total.borrower
   (:require
-    [clojure.spec.alpha :as s]
-    [orchestra.spec.test :as ostest]))
+   [clojure.spec.alpha :as s]
+   [orchestra.spec.test :as ostest]))
 
 (s/def ::name string?)
 (s/def ::max-books (s/and int? #(>= % 0)))
@@ -10,40 +10,40 @@
 (defn make-borrower [name max-books]
   {::name name, ::max-books max-books})
 (s/fdef make-borrower
-        :args (s/cat :name ::name
-                     :max-books ::max-books)
-        :ret ::borrower)
+  :args (s/cat :name ::name
+               :max-books ::max-books)
+  :ret ::borrower)
 
 (defn get-name [borrower]
   (borrower ::name))
 (s/fdef get-name
-        :args (s/cat :borrower ::borrower)
-        :ret ::name)
+  :args (s/cat :borrower ::borrower)
+  :ret ::name)
 
 (defn set-name [borrower name]
   (assoc borrower ::name name))
 (s/fdef set-name
-        :args (s/cat :borrower ::borrower
-                     :name ::name)
-        :ret ::borrower)
+  :args (s/cat :borrower ::borrower
+               :name ::name)
+  :ret ::borrower)
 
 (defn get-max-books [borrower]
   (borrower ::max-books))
 (s/fdef get-max-books
-        :args (s/cat :borrower ::borrower)
-        :ret ::max-books)
+  :args (s/cat :borrower ::borrower)
+  :ret ::max-books)
 
 (defn set-max-books [borrower max-books]
   (assoc borrower ::max-books max-books))
 (s/fdef set-max-books
-        :args (s/cat :borrower ::borrower
-                     :max-books ::max-books)
-        :ret ::borrower)
+  :args (s/cat :borrower ::borrower
+               :max-books ::max-books)
+  :ret ::borrower)
 
 (defn borrower-to-string [borrower]
   (str (get-name borrower) " (" (get-max-books borrower) " books)"))
 (s/fdef borrower-to-string
-        :args (s/cat :borrower ::borrower)
-        :ret string?)
+  :args (s/cat :borrower ::borrower)
+  :ret string?)
 
 (ostest/instrument)
