956efd9b622591aaf8f840030be5ed61161449ed
Eric Tobin <eatobin@gmail.com>
2021-03-11 21:52:35 -0600

    borrower progress

--- test/total/borrower_test.clj
+++ test/total/borrower_test.clj
@@ -4,27 +4,23 @@
             [total.borrower :as br]
             [clojure.spec.alpha :as s]))
 
-;;(def br1 (br/make-borrower "Borrower1" 1))
-;;(s/conform :unq/borrower
-;;           (br/make-borrower "Borrower1" 1))
-;
 (def json-string-br "{\"name\":\"Borrower1\",\"maxBooks\":1}")
 (def br1 (br/borrower-json-string-to-borrower json-string-br))
-(println br1)
-;
-;
-;(deftest get-name-test
-;  (is (= "Borrower1"
-;         (br/get-name br1))))
-;(s/conform ::dom/name
-;           (br/get-name br1))
-;
-;(deftest get-max-books-test
-;  (is (= 1
-;         (br/get-max-books br1))))
-;(s/conform ::dom/max-books
-;           (br/get-max-books br1))
-;
+(s/conform :unq/borrower
+           br1)
+
+(deftest get-name-test
+  (is (= "Borrower1"
+         (br/get-name br1))))
+(s/conform ::dom/name
+           (br/get-name br1))
+
+(deftest get-max-books-test
+  (is (= 1
+         (br/get-max-books br1))))
+(s/conform ::dom/max-books
+           (br/get-max-books br1))
+
 ;(deftest set-name-test
 ;  (is (= {:name "Jack", :max-books 1}
 ;         (br/set-name br1 "Jack"))))
