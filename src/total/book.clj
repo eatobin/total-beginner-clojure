5faf218f1d7f8f592b4cdf65b6e52813d29d2117
Eric Tobin <eatobin@gmail.com>
2021-03-12 13:42:35 -0600

    book tests

--- src/total/book.clj
+++ src/total/book.clj
@@ -10,51 +10,47 @@
 (s/def get-title
   ::dom/extract-fn-bk-title)
 
-;(defn get-author [book]
-;  (:author book))
-;(s/fdef get-author
-;        :args (s/cat :book :unq/book)
-;        :ret ::dom/author)
-;
-;(defn get-borrower [book]
-;  (:maybe-borrower book))
-;(s/fdef get-borrower
-;        :args (s/cat :book :unq/book)
-;        :ret ::dom/maybe-borrower)
-;
-;(defn set-borrower [book borrower]
-;  (assoc book :maybe-borrower borrower))
-;(s/fdef set-borrower
-;        :args (s/cat :book :unq/book
-;                     :borrower ::dom/maybe-borrower)
-;        :ret :unq/book)
-;
-;(defn- available-string [book]
-;  (let [borrower (get-borrower book)]
-;    (if (nil? borrower)
-;      "Available"
-;      (str
-;        "Checked out to "
-;        (br/get-name borrower)))))
-;(s/fdef available-string
-;        :args (s/cat :book :unq/book)
-;        :ret string?)
-;
-;(defn book-to-string [book]
-;  (str
-;    (get-title book)
-;    " by "
-;    (get-author book)
-;    "; "
-;    (available-string book)))
-;(s/fdef book-to-string
-;        :args (s/cat :book :unq/book)
-;        :ret string?)
-;
-;def bookJsonStringToBook(bookString: String): Either[Error, Book] = decode[Book](bookString)
-;
-;def bookToJsonString(bk: Book): JsonString =
-;bk.asJson.noSpaces
+(defn get-author [book]
+  (:author book))
+(s/fdef get-author
+        :args (s/cat :book :unq/book)
+        :ret ::dom/author)
+
+(defn get-borrower [book]
+  (:maybe-borrower book))
+(s/fdef get-borrower
+        :args (s/cat :book :unq/book)
+        :ret ::dom/maybe-borrower)
+
+(defn set-borrower [book borrower]
+  (assoc book :maybe-borrower borrower))
+(s/fdef set-borrower
+        :args (s/cat :book :unq/book
+                     :borrower ::dom/maybe-borrower)
+        :ret :unq/book)
+
+(defn- available-string [book]
+  (let [borrower (get-borrower book)]
+    (if (nil? borrower)
+      "Available"
+      (str
+        "Checked out to "
+        (br/get-name borrower)))))
+(s/fdef available-string
+        :args (s/cat :book :unq/book)
+        :ret string?)
+
+(defn to-string [book]
+  (str
+    (get-title book)
+    " by "
+    (get-author book)
+    "; "
+    (available-string book)))
+(s/fdef to-string
+        :args (s/cat :book :unq/book)
+        :ret string?)
+
 (defn- my-key-reader
   [key]
   (cond
@@ -75,5 +71,12 @@
 (s/fdef book-json-string-to-book
         :args (s/cat :book-string string?)
         :ret :unq/book)
+
+(defn book-to-json-string [book]
+  (json/write-str book
+                  :key-fn my-key-writer))
+(s/fdef book-to-json-string
+        :args (s/cat :book :unq/book)
+        :ret string?)
 
 (ostest/instrument)
--- test/total/book_test.clj
+++ test/total/book_test.clj
@@ -5,73 +5,76 @@
             [clojure.spec.alpha :as s]))
 
 
-(def jsonStringBk1 "{\"title\":\"Title1\",\"author\":\"Author1\",\"borrower\":null}")
-(def jsonStringBk2 "{\"title\":\"Title1\",\"author\":\"Author1\",\"borrower\":{\"name\":\"Borrower2\",\"maxBooks\":2}}")
+(def json-string-bk1 "{\"title\":\"Title1\",\"author\":\"Author1\",\"borrower\":null}")
+(def json-string-bk2 "{\"title\":\"Title1\",\"author\":\"Author1\",\"borrower\":{\"name\":\"Borrower2\",\"maxBooks\":2}}")
+(def json-string-bees "{\"title\":\"Title1\",\"bees\":\"Knees\",\"author\":\"Author1\",\"borrower\":null}")
+;(def json-string-short "{\"title\":\"Title1\",\"borrower\":null}") //fails spec - no Author
 (def br2 {:name "Borrower2" :max-books 2})
 
-;(def bk1 (bk/make-book "Title1" "Author1"))
-;(def bk2 (bk/set-borrower bk1 br2))
-;
-;private val jsonStringBk1: String = "{\"title\":\"Title1\",\"author\":\"Author1\",\"borrower\":null}"
-;private val jsonStringBk2: String = "{\"title\":\"Title1\",\"author\":\"Author1\",\"borrower\":{\"name\":\"Borrower2\",\"maxBooks\":2}}"
-;private val br2: Borrower = Borrower("Borrower2", 2)
-;private val bk1: Book = bookJsonStringToBook(jsonStringBk1).getOrElse(Book("", ""))
-;private val bk2: Book = setBorrower(Some(br2), bk1)
-;private val bk3: Book = Book("Title3", "Author3")
-;
-;
-;
-;(s/conform :unq/book
-;           (bk/make-book "Title1" "Author1"))
-;(s/conform :unq/book
-;           (bk/make-book "Title1" "Author1" br2))
-;
-;(s/conform (s/cat :title ::dom/title
-;                  :author ::dom/author
-;                  :borrower (s/? ::dom/maybe-borrower))
-;           ["t" "a"])
-;
-;(s/conform (s/cat :title ::dom/title
-;                  :author ::dom/author
-;                  :borrower (s/? ::dom/maybe-borrower))
-;           ["t" "a" nil])
-;
-;(s/conform (s/cat :title ::dom/title
-;                  :author ::dom/author
-;                  :borrower (s/? ::dom/maybe-borrower))
-;           ["t" "a" br2])
-;
-;(deftest get-title-test
-;  (is (= "Title1"
-;         (bk/get-title bk1))))
-;(s/conform ::dom/title
-;           (bk/get-title bk1))
-;
-;(deftest get-author-test
-;  (is (= "Author1"
-;         (bk/get-author bk1))))
-;(s/conform ::dom/author
-;           (bk/get-author bk1))
-;
-;(deftest get-borrower-nil-test
-;  (is (nil? (bk/get-borrower bk1))))
-;(s/conform ::dom/maybe-borrower
-;           (bk/get-borrower bk1))
-;
-;(deftest get-borrower-someone-test
-;  (is (= br2
-;         (bk/get-borrower bk2))))
-;(s/conform ::dom/maybe-borrower
-;           (bk/get-borrower bk2))
-;
-;(deftest book-to-string-nil-test
-;  (is (= "Title1 by Author1; Available"
-;         (bk/book-to-string bk1))))
-;(s/conform string?
-;           (bk/book-to-string bk1))
-;
-;(deftest book-to-string-someone-test
-;  (is (= "Title1 by Author1; Checked out to Borrower2"
-;         (bk/book-to-string bk2))))
-;(s/conform string?
-;           (bk/book-to-string bk2))
+(def bk1 (bk/book-json-string-to-book json-string-bk1))
+(def bk2 (bk/set-borrower bk1 br2))
+(def bk-bees (bk/book-json-string-to-book json-string-bees))
+
+(s/conform (s/cat :title ::dom/title
+                  :author ::dom/author
+                  :borrower (s/? ::dom/maybe-borrower))
+           ["t" "a"])
+
+(s/conform (s/cat :title ::dom/title
+                  :author ::dom/author
+                  :borrower (s/? ::dom/maybe-borrower))
+           ["t" "a" nil])
+
+(s/conform (s/cat :title ::dom/title
+                  :author ::dom/author
+                  :borrower (s/? ::dom/maybe-borrower))
+           ["t" "a" br2])
+
+(deftest get-title-test
+  (is (= "Title1"
+         (bk/get-title bk1))))
+(s/conform ::dom/title
+           (bk/get-title bk1))
+
+(deftest get-author-test
+  (is (= "Author1"
+         (bk/get-author bk1))))
+(s/conform ::dom/author
+           (bk/get-author bk1))
+
+(deftest get-borrower-nil-test
+  (is (nil? (bk/get-borrower bk1))))
+(s/conform ::dom/maybe-borrower
+           (bk/get-borrower bk1))
+
+(deftest get-borrower-someone-test
+  (is (= br2
+         (bk/get-borrower bk2))))
+(s/conform ::dom/maybe-borrower
+           (bk/get-borrower bk2))
+
+(deftest to-string-nil-test
+  (is (= "Title1 by Author1; Available"
+         (bk/to-string bk1))))
+(s/conform string?
+           (bk/to-string bk1))
+
+(deftest to-string-someone-test
+  (is (= "Title1 by Author1; Checked out to Borrower2"
+         (bk/to-string bk2))))
+(s/conform string?
+           (bk/to-string bk2))
+
+(deftest book-to-json-string
+  (is (= json-string-bk1
+         (bk/book-to-json-string bk1)))
+  (is (= json-string-bk2
+         (bk/book-to-json-string bk2)))
+  (is (= json-string-bees
+         (bk/book-to-json-string bk-bees))))
+(s/conform string?
+           (bk/book-to-json-string bk1))
+(s/conform string?
+           (bk/book-to-json-string bk2))
+(s/conform string?
+           (bk/book-to-json-string bk-bees))
