91e4a60d56e22c7ebdeaff44a6fef60b86f2a2f7
Eric Tobin <eatobin@gmail.com>
2022-02-26 22:07:32 -0800

    malli progress

--- src/total/borrower.clj
+++ src/total/borrower.clj
@@ -1,10 +1,6 @@
 (ns total.borrower
   (:require
-    [total.domain :as dom]
     [clojure.data.json :as json]
-    [clojure.spec.alpha :as s]
-    [orchestra.spec.test :as ostest]
-    [malli.core :as m]
     [malli.instrument :as mi]))
 
 (def =>bn [:string {:min 1}])
@@ -13,6 +9,11 @@
 
 (def =>get-name [:=> [:cat =>br] =>bn])
 (def =>set-name [:=> [:cat =>br =>bn] =>br])
+(def =>get-max-books [:=> [:cat =>br] =>mb])
+(def =>set-max-books [:=> [:cat =>br =>mb] =>br])
+(def =>to-string [:=> [:cat =>br] :string])
+(def =>borrower-json-string-to-borrower [:=> [:cat :string] =>br])
+(def =>borrower-to-json-string [:=> [:cat =>br] :string])
 
 (defn get-name
   "get the name of a borrower"
@@ -25,29 +26,24 @@
   {:malli/schema =>set-name}
   [borrower name]
   (assoc borrower :name name))
-;(s/fdef set-name
-;        :args (s/cat :borrower :unq/borrower
-;                     :name ::dom/name)
-;        :ret :unq/borrower)
 
-(defn get-max-books [borrower]
+(defn get-max-books
+  "get the max-books of a borrower"
+  {:malli/schema =>get-max-books}
+  [borrower]
   (:max-books borrower))
-(s/fdef get-max-books
-        :args (s/cat :borrower :unq/borrower)
-        :ret ::dom/max-books)
 
-(defn set-max-books [borrower max-books]
+(defn set-max-books
+  "set the max-books of a borrower"
+  {:malli/schema =>set-max-books}
+  [borrower max-books]
   (assoc borrower :max-books max-books))
-(s/fdef set-max-books
-        :args (s/cat :borrower :unq/borrower
-                     :max-books ::dom/max-books)
-        :ret :unq/borrower)
 
-(defn to-string [borrower]
+(defn to-string
+  "send a borrower to-string"
+  {:malli/schema =>to-string}
+  [borrower]
   (str (get-name borrower) " (" (get-max-books borrower) " books)"))
-(s/fdef to-string
-        :args (s/cat :borrower :unq/borrower)
-        :ret string?)
 
 (defn- my-key-reader
   [key]
@@ -61,20 +57,19 @@
     (= key :max-books) "maxBooks"
     :else (name key)))
 
-(defn borrower-json-string-to-borrower [borrower-string]
+(defn borrower-json-string-to-borrower
+  "create a borrower from a JSON string"
+  {:malli/schema =>borrower-json-string-to-borrower}
+  [borrower-string]
   (json/read-str borrower-string
                  :key-fn my-key-reader))
-(s/fdef borrower-json-string-to-borrower
-        :args (s/cat :borrower-string string?)
-        :ret :unq/borrower)
 
-(defn borrower-to-json-string [borrower]
+(defn borrower-to-json-string
+  "create a JSON string from a borrower"
+  {:malli/schema =>borrower-to-json-string}
+  [borrower]
   (json/write-str borrower
                   :key-fn my-key-writer))
-(s/fdef borrower-to-json-string
-        :args (s/cat :borrower :unq/borrower)
-        :ret string?)
 
-(ostest/instrument)
 (mi/collect!)
 (mi/instrument!)
